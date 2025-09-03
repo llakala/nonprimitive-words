b4fa493bf0818077d8f47a8f8faa29e5c5a5be09
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-03 09:39:29 -0400

    llakala/pumpkinpie: move to subdir

--- llakala/project.nix
+++ llakala/project.nix
@@ -2,25 +2,8 @@
   pkgs,
   system,
   ...
-}: let
-  python3Packages = pkgs.python3Packages;
-in {
-  packages.${system} = let
-    pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
-  in {
-    ${pyproject.project.name} = (
-      python3Packages.buildPythonApplication {
-        src = ./.;
-        pname = pyproject.project.name;
-        version = pyproject.project.version;
-        pyproject = true;
-        build-system = with python3Packages; [setuptools];
-
-        meta = {
-          description = pyproject.project.description;
-          mainProgram = pyproject.project.name;
-        };
-      }
-    );
+}: {
+  packages.${system} = {
+    pumpkinpie = import ./pumpkinpie {inherit pkgs;};
   };
 }
