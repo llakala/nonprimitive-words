1ba96d1be0ed478593858a53252ef282ebbb6e07
Notarin Steele <424c414e4b@gmail.com>
2025-09-03 09:25:40 -0400

    ocicat: renamed to ocicat

--- Notarin/project.nix
+++ Notarin/project.nix
@@ -6,15 +6,16 @@
 }: let
   craneLib = crane.mkLib pkgs;
 in {
-  packages.${system} = {
-    notarin-rust = craneLib.buildPackage {
-      src = ./rust;
+  packages.${system} = let
+    ocicatCargo = builtins.fromTOML (builtins.readFile ./ocicat/Cargo.toml);
+  in {
+    ${ocicatCargo.package.name} = craneLib.buildPackage {
+      src = ./ocicat;
       nativeBuildInputs = with pkgs; [
         pkg-config
       ];
       meta = {
         description = "A rust algorithm solving A239019.";
-        mainProgram = "A239019";
       };
     };
   };
