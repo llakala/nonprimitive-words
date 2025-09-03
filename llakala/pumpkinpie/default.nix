33eac32f3f54795b9e65b8831436f0818f8537a5
Notarin Steele <424c414e4b@gmail.com>
2025-09-03 12:07:21 -0400

    pumpkinpie: improved some practices

--- llakala/pumpkinpie/default.nix
+++ llakala/pumpkinpie/default.nix
@@ -1,16 +1,16 @@
 {pkgs}: let
-  python3Packages = pkgs.python3Packages;
+  inherit (pkgs) python3Packages;
   pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
 in
   python3Packages.buildPythonApplication {
     src = ./.;
     pname = pyproject.project.name;
-    version = pyproject.project.version;
+    inherit (pyproject.project) version;
     pyproject = true;
     build-system = with python3Packages; [setuptools];
 
     meta = {
-      description = pyproject.project.description;
+      inherit (pyproject.project) description;
       mainProgram = pyproject.project.name;
     };
   }
