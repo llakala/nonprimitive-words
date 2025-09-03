c78503e2cda149a5b30f03447fbf82ede7868cbd
NotAShelf <raf@notashelf.dev>
2025-09-03 21:41:05 +0300

    notashelf: update nix entrypoint for new package
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6a6a696440a415a2841bb9f74deb213e24d47d83

--- notashelf/project.nix
+++ notashelf/project.nix
@@ -4,6 +4,42 @@
   ...
 }: {
   packages.x86_64-linux = {
+    "brutalist" = let
+      fs = lib.fileset;
+    in
+      pkgs.stdenv.mkDerivation {
+        pname = "brutalist";
+        version = "0-unstable-2025-09-03";
+
+        src = fs.toSource {
+          root = ./brutalist;
+          fileset = fs.unions [./brutalist/main.c];
+        };
+
+        buildPhase = ''
+          runHook preBuild
+
+          ${lib.getExe pkgs.stdenv.cc} -O3 -fomit-frame-pointer -pipe -DNDEBUG main.c -o brutalist
+
+          runHook postBuild
+        '';
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/bin
+          install -Dm755 brutalist $out/bin
+
+          runHook postInstall
+        '';
+
+        meta = {
+          description = "C99-compatible implementation for OEIS A239019";
+          mainProgram = "steampunk";
+          maintainers = [lib.maintainers.NotAShelf];
+        };
+      };
+
     "steampunk" = let
       fs = lib.fileset;
     in
@@ -12,7 +48,7 @@
         version = "0-unstable-2025-09-03";
 
         src = fs.toSource {
-          root = ./steampunk/.;
+          root = ./steampunk;
           fileset = fs.unions [./steampunk/main.asm];
         };
 
