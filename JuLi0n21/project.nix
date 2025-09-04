3b670e5365420f5b4fe66865f5feeacd1b3acee8
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-04 16:58:04 -0400

    JuLi0n21/strawberry: add meta.mainProgram

--- JuLi0n21/project.nix
+++ JuLi0n21/project.nix
@@ -7,6 +7,10 @@
       src = ./strawberry;
 
       vendorHash = null;
+
+      # Binary that gets run when doing `nix run .#strawberry`. Same as the name
+      # of the go module
+      meta.mainProgram = "nonprimitivewords";
     };
   };
 }
